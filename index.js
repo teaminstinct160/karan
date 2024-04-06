5bdeb5bf25ac7ae534316c2ec45e06d0b9650c79
Karan Pratap Sen <daradioactivemind@gmail.com>
2024-04-06 11:56:11 +0545

    First Version commit

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/home/karan/.nvm/versions/node/v21.7.1/bin/node
+const clearConsole = require('clear-any-console');
+clearConsole()
+console.log(`
+Karan Sen - Web Developer
+
+Fullstack web development
+
+    Twitter: https://twitter.com/atheistinstinct
+    GitHub: https://github.com/teaminstinct160 
+`);
